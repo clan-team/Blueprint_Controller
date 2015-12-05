fbd1fbaee6e26f5e710af7bd43f16225d3b5db49
Annika Erickson <nika.bryn@gmail.com>
2015-12-05 12:26:32 -0800

    Initial Commit

---
+++ blueprint/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  blueprint
+//
+//  Created by Annika Erickson on 12/5/15.
+//  Copyright (c) 2015 Claan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
